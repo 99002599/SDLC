92902ab0df5f7b52def6dc0b9fd4d49f9ff85106
99002599 <72463292+99002599@users.noreply.github.com>
2020-10-06 22:43:55 +0530

    Add files via upload

---
+++ header_try.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_TRY_H_INCLUDED
+#define HEADER_TRY_H_INCLUDED
+
+char dataToBeWritten[50]
+		= "Be Grateful!!!!!";
+
+#endif // HEADER_TRY_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,11 @@
+// C program to Open a File,
+// Write in it, And Close the File
+
+# include <stdio.h>
+
+void test();
+int main(void)
+{
+    test();
+	return 0;
+}
